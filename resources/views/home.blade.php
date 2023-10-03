c0acee9922b1fafc6a92b467e71d090052536683
Luigi Hernandez <luigijhernandez7575@gmail.com>
2023-10-02 20:31:02 -0400

    cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,35 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <div class="col-lg-4 col-xs-6">
+
+        <div class="card bg-purple rounded shadow">
+            <div class="card-body">
+                <div class="d-flex no-block align-items-center">
+                    <div class="text-white">
+                        <h2 class="text-white font-weight-bold">
+                            <a href="{{ route('people.index') }}" class="text-white text-bold">
+                                {{ $people->count() }}
+                            </a>
+                        </h2>
+                        <h6 class="text-white">
+                            <a href="{{ route('people.index') }}" class="text-white">
+                                Personas Registradas
+                            </a>
+                        </h6>
+                    </div>
+                    <div class="ml-auto">
+                        <span class="text-white display-6">
+                            <a href="{{ route('people.index') }}" class="text-white">
+                                <i class="fas fa-users"></i>
+                            </a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 @stop
 
 @section('css')
